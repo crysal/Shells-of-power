69e338a55aaa77256cfda3725b7d9b0a12d7e504
crysal0 <45420554+crysal0@users.noreply.github.com>
2019-01-17 08:27:59 +0100

    Add files via upload

---
+++ Epoxi.ps1
@@ -0,0 +1,14 @@
+﻿$HowManyNames = (Get-ChildItem | Where-Object {$_.Mode -gt 'd'}).FullName
+$ChildItems = (Get-ChildItem $HowManyNames | Where-Object {$_.Mode -gt 'd'}).FullName
+$Combi = $HowManyNames + $ChildItems
+$ChildItems2 = (Get-ChildItem $ChildItems | Where-Object {$_.Mode -gt 'd'}).FullName
+$Combi2 = $Combi + $ChildItems2
+$HowMany = ($Combi2).Count
+$UnixEpoch = '1. january 1970 00:00:00'
+while ($HowMany -ne 0)
+{
+$HowMany--
+((Get-Item $Combi2[$HowMany]).CreationTime) = $UnixEpoch
+((Get-Item $Combi2[$HowMany]).LastWriteTime) = $UnixEpoch
+((Get-Item $Combi2[$HowMany]).LastAccessTime) = $UnixEpoch
+}
