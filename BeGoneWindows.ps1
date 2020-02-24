ea63c61953f6c4ae8f725278c0e579af93fde670
crysal0 <45420554+crysal0@users.noreply.github.com>
2020-02-24 09:50:36 +0100

    Update BeGoneWindows.ps1

--- BeGoneWindows.ps1
+++ BeGoneWindows.ps1
@@ -1,74 +1,74 @@
-Get-AppxPackage -AllUsers -Name Microsoft.3DBuilder | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Getstarted | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.SkypeApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsMaps | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.BingWeather | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Office.OneNote | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.ZuneMusic | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.ZuneVideo | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.BingSports | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.BingNews | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsPhone | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.BingFinance | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsCamera | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.People | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.AAD.BrokerPlugin | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.LockApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftEdgeDevToolsClient | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftEdge | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Windows.CapturePicker | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Windows.CloudExperienceHost | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Windows.ContentDeliveryManager | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Windows.Cortana | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Windows.NarratorQuickStart | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Windows.ParentalControls | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxGameCallableUI | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Windows.CBSPreview | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name windows.immersivecontrolpanel | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Windows.PrintDialog | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Wallet | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WebpImageExtension | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Xbox.TCUI | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Getstarted | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxGameOverlay | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Messaging | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsCamera | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxGamingOverlay | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Print3D | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsMaps | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.ScreenSketch | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.ZuneMusic | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.People | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.SkypeApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsStore | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MixedReality.Portal | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Advertising. | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WebMediaExtensions | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Advertising.Xaml | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxIdentityProvider | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name microsoft.windowscommunicationsapps | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.YourPhone | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.VP9VideoExtensions | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.WindowsFeedbackHub | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Services.Store.Engagement | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Services.Store.Engagement | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.DesktopAppInstaller | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.StorePurchaseApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.HEIFImageExtension | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.OneConnect | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.XboxApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.GetHelp | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.ZuneVideo | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.BingWeather | Remove-AppxPackage
-Get-AppxPackage -AllUsers -Name Microsoft.Office.OneNote | Remove-AppxPackage
+Get-AppxPackage -AllUsers -Name Microsoft.3DBuilder | Remove-AppxPackage	#	View, create, and personalize 3D objects using 3D Builder.
+Get-AppxPackage -AllUsers -Name Microsoft.Advertising. | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Advertising.Xaml | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.BingFinance | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.BingNews | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.BingSports | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.BingWeather | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.BingWeather | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.DesktopAppInstaller | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.GetHelp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Getstarted | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Getstarted | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.HEIFImageExtension | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.LockApp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Messaging | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftEdge | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftEdgeDevToolsClient | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftOfficeHub | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftOfficeHub | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MicrosoftStickyNotes | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.MixedReality.Portal | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Office.OneNote | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Office.OneNote | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.OneConnect | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.People | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.People | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Print3D | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.ScreenSketch | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Services.Store.Engagement | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Services.Store.Engagement | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.SkypeApp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.SkypeApp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.StorePurchaseApp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.VP9VideoExtensions | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Wallet | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WebMediaExtensions | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WebpImageExtension | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Windows.CapturePicker | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Windows.CloudExperienceHost | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Windows.ContentDeliveryManager | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Windows.Cortana | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Windows.NarratorQuickStart | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Windows.ParentalControls | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsAlarms | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsAlarms | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsCamera | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsCamera | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name microsoft.windowscommunicationsapps | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsFeedbackHub | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsMaps | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsMaps | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsPhone | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsSoundRecorder | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsSoundRecorder | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.WindowsStore | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.Xbox.TCUI | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxApp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxApp | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxGameCallableUI | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxGameOverlay | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxGamingOverlay | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxIdentityProvider | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.YourPhone | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.ZuneMusic | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.ZuneMusic | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.ZuneVideo | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.ZuneVideo | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Microsoft.AAD.BrokerPlugin | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Windows.CBSPreview | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name windows.immersivecontrolpanel | Remove-AppxPackage	#	
+Get-AppxPackage -AllUsers -Name Windows.PrintDialog | Remove-AppxPackage	#	
