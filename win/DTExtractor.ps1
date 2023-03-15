7e4afda6b578be263ae551a2c9fe1a11c20f9431
Crysal <45420554+crysal@users.noreply.github.com>
2023-03-15 16:17:21 +0100

    Update DTExtractor.ps1

--- win/DTExtractor.ps1
+++ win/DTExtractor.ps1
@@ -4,7 +4,7 @@ foreach ($folder in $PFolders)
 $ListOFiles=Get-ChildItem -Name "C:\Program Files (x86)\Steam\steamapps\common\Warhammer 40,000 DARKTIDE\bundle\data\$folder\*.STREAM"
 foreach ($item in $ListOFiles){
 if ( -not (Test-Path "U:\ripaudio\extract\$item" -PathType Container)){
-.\RavioliGameTools_v2.10\RScannerConsole.exe "C:\Program Files (x86)\Steam\steamapps\common\Warhammer 40,000 DARKTIDE\bundle\data\$folder\$item" /e:U:\ripaudio\extract\$item
+.\RavioliGameTools\RScannerConsole.exe "C:\Program Files (x86)\Steam\steamapps\common\Warhammer 40,000 DARKTIDE\bundle\data\$folder\$item" /e:U:\ripaudio\extract\$item
 if ( -not (Test-Path "U:\ripaudio\extract\$item" -PathType Container)){
 mkdir "U:\ripaudio\extract\$item"
 }
